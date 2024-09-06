f97393e12257035335907da98025476582d12404
CallMeShepard <777loginovdanja777@gmail.com>
2024-09-06 12:32:34 +0300

    Update code, p.7

--- Lab1/Lab1_code.java
+++ Lab1/Lab1_code.java
@@ -1,5 +1,6 @@
 class Lab1 {
   public static void main(String[] args) {
-    System.out.println(“Its works”);
+    System.out.println("It just works, buy Starfield");
+	  System.out.println(":: yeah so you can play it with two hands or with only one. It was important to us that you be able to play it with one hand in case you need the other hand free… to drink morning coffee or something not what you guys are thinking… degenerates...");
   }
 }
