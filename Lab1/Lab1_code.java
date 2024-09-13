cfc30ea9981ce179dd13dcd0b7145084d8556ede
CallMeShepard <777loginovdanja777@gmail.com>
2024-09-13 10:11:06 +0300

    Update again

--- Lab1/Lab1_code.java
+++ Lab1/Lab1_code.java
@@ -3,4 +3,4 @@ class Lab1 {
     System.out.println("It just works, buy Starfield");
 	  System.out.println(":: yeah so you can play it with two hands or with only one. It was important to us that you be able to play it with one hand in case you need the other hand free… to drink morning coffee or something not what you guys are thinking… degenerates...");
   }
-}
+} 
